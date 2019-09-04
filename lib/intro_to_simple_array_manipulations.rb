da8d78ef21c7b2d565875e358595e8f6c3381be2
Lialah Barrer <lrbarrer@gmail.com>
2019-09-04 01:30:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,7 @@ def using_concat (array, element)
   array.concat(element)
 end
 
-def using_insert (array, element)
+def using_insert (array)
   array = [2,3,4,5,6,7]
   array.insert(4, "deadpool")
 end
